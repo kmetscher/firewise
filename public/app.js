b148ba94cc12d3576e2cb1fd5058585050fce80e
Kyle Metscher <kscott.mets@gmail.com>
2023-02-02 11:40:46 -0800

    prod vue

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js";
 import * as leaflet from 'https://cdn.skypack.dev/leaflet'
 import makeVisible from "./observer.js";
 
